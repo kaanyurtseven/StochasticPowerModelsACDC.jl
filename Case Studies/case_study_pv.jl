32a3d2f37de20771e39dfd97157f62c026585728
kaanyurtseven <eng.kaanyurtseven@gmail.com>
2022-12-18 13:44:21 +0100

    Bug fixing

--- Case Studies/case_study_pv.jl
+++ Case Studies/case_study_pv.jl
@@ -24,7 +24,7 @@ Memento.setlevel!(Memento.getlogger(PowerModelsACDC), "error")
 Memento.setlevel!(Memento.getlogger(PowerModels), "error")
 
 #Solver inputs
-ipopt_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "print_level"=>0, "max_iter"=>5000, "sb"=>"yes", "fixed_variable_treatment" => "relax_bounds")
+ipopt_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "print_level"=>0, "max_iter"=>2000, "sb"=>"yes", "fixed_variable_treatment" => "relax_bounds")
 
 #gPC degree input
 deg  = 2
@@ -55,6 +55,7 @@ case1 = "case67_AC_SPMACDC_95cc.m"
 file1  = joinpath(BASE_DIR, "test/data/matpower/Case Studies", case1)
 
 case2 = "case67_ACDC_SPMACDC_95cc.m"
+# case2 = "case5_ACDC_SPMACDC.m"
 file2  = joinpath(BASE_DIR, "test/data/matpower/Case Studies", case2)
 
 set = Dict("output" => Dict("duals" => false, "branch_flows" => true), "conv_losses_mp" => true)
@@ -62,61 +63,69 @@ data = _PM.parse_file(file2)
 
 #Penetration level iteration
 for pen_level = pen_level_start:pen_level_step:pen_level_end
-    println("Penetration Level = $pen_level")
     
-    total_load = sum([data["load"]["$i"]["pd"] for i=1:length(data["load"])])
-    p_size = pen_level * total_load / length(data["load"]) #Calculate PV size for each load bus
-    
-    if solve_case1
-        global result_spmacdc_case1 = solve_sopf_acdc_PV(file1, _PM.IVRPowerModel, ipopt_solver, deg=deg, p_size=p_size);
-    end
+    if solve_case1 || solve_case2
+        println("Penetration Level = $pen_level")
+        
+        total_load = sum([data["load"]["$i"]["pd"] for i=1:length(data["load"])])
+        p_size = pen_level * total_load / length(data["load"]) #Calculate PV size for each load bus
+        p_size_dict[pen_level] = p_size
+        
+        if solve_case1
+            global result_spmacdc_case1 = solve_sopf_acdc_PV(file1, _PM.IVRPowerModel, ipopt_solver, deg=deg, p_size=p_size);
+            
+            #Store necessary values for reporting
+            obj_case1[pen_level] = result_spmacdc_case1["objective"]
+            stat_case1[pen_level] = string(result_spmacdc_case1["primal_status"])
 
-    if solve_case2
-        global result_spmacdc_case2 = solve_sopf_acdc_PV(file2, _PM.IVRPowerModel, ipopt_solver, deg=deg, p_size=p_size);
-    end
+            if string(result_spmacdc_case1["primal_status"]) != "FEASIBLE_POINT" && string(result_spmacdc_case1["primal_status"]) !="NEARLY_FEASIBLE_POINT"
+                global feas_ctr1 += 1
+            else
+                global feas_ctr1 = 0
+            end
 
-    #Store necessary values for reporting
-    obj_case1[pen_level] = result_spmacdc_case1["objective"] 
-    obj_case2[pen_level] = result_spmacdc_case2["objective"] 
-    stat_case1[pen_level] = string(result_spmacdc_case1["primal_status"])
-    stat_case2[pen_level] = string(result_spmacdc_case2["primal_status"])
-    p_size_dict[pen_level] = p_size
+            if feas_ctr1 >= 2
+                global solve_case1 = false
+                println("   Case 1 reached infeasibility on penetration level of $pen_level.")
+            end
 
-    if string(result_spmacdc_case1["primal_status"]) != "FEASIBLE_POINT" && string(result_spmacdc_case1["primal_status"]) !="NEARLY_FEASIBLE_POINT"
-        global feas_ctr1 += 1
-    else
-        global feas_ctr1 = 0
-    end
+        end
 
-    if string(result_spmacdc_case2["primal_status"]) != "FEASIBLE_POINT" && string(result_spmacdc_case2["primal_status"]) !="NEARLY_FEASIBLE_POINT"
-        global feas_ctr2 += 1
-    else
-        global feas_ctr2 = 0
-    end
+        if solve_case2
+            global result_spmacdc_case2 = solve_sopf_acdc_PV(file2, _PM.IVRPowerModel, ipopt_solver, deg=deg, p_size=p_size);
+            
+            #Store necessary values for reporting
+            obj_case2[pen_level] = result_spmacdc_case2["objective"] 
+            stat_case2[pen_level] = string(result_spmacdc_case2["primal_status"])
 
-    if feas_ctr1 >= 2
-        global solve_case1 = false
-        println("Case 1 reached infeasibility on penetration level of $pen_level.")
-    end
+            if string(result_spmacdc_case2["primal_status"]) != "FEASIBLE_POINT" && string(result_spmacdc_case2["primal_status"]) !="NEARLY_FEASIBLE_POINT"
+                global feas_ctr2 += 1
+            else
+                global feas_ctr2 = 0
+            end
 
-    if feas_ctr2 >= 2
-        println("Case 2 reached infeasibility on penetration level of $pen_level.")
-        global solve_case2 = false
+            if feas_ctr2 >= 2
+                println("   Case 2 reached infeasibility on penetration level of $pen_level.")
+                global solve_case2 = false
+            end
+
+        end
+
     end
 
 end
 
 
 #Show results on the terminal
-println("\n\n>>> SPMACDC Results - Case 1 >>>")
-println(result_spmacdc_case1["primal_status"])
-print("Objective: ")
-print(result_spmacdc_case1["objective"])
+# println("\n\n>>> SPMACDC Results - Case 1 >>>")
+# println(result_spmacdc_case1["primal_status"])
+# print("Objective: ")
+# print(result_spmacdc_case1["objective"])
 
-println("\n\n>>> SPMACDC Results - Case 2 >>>")
-println(result_spmacdc_case2["primal_status"])
-print("Objective: ")
-print(result_spmacdc_case2["objective"])
+# println("\n\n>>> SPMACDC Results - Case 2 >>>")
+# println(result_spmacdc_case2["primal_status"])
+# print("Objective: ")
+# print(result_spmacdc_case2["objective"])
 
 # Reporting
 if Report
